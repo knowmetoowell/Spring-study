63bb72a43173ade54e8b5264be204d2fdb5839a8
knowmetoowell <kumct12@naver.com>
2021-12-08 23:16:48 +0900

    spring-study 2일차
    정적 컨텐츠, MVC와 템플릿 엔진, API / 회원 관리 예제

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,6 +13,38 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
 
+    //hello-template html
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute( "name", name);
+        return "hello-template";
+
+    }
+
+    //그대로 데이터를 내려줌
+    @GetMapping("hello-string")
+    @ResponseBody //@ResponseBody 를 사용하면 뷰 리졸버( viewResolver )를 사용하지 않음
+    public String helloString(@RequestParam("name") String name){
+        return  "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        //단축기 alt + insert => getter, setter
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
