d5e303ded94b941e15ade6b8ca3856244ec2c9be
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-07-29 02:17:51 +0900

    오류 코드 수정

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -19,8 +19,7 @@ public class OrderApp {
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
-        MemberService orderService = applicationContext.getBean("orderService", OrderService.class);
-
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "mamberA", Grade.VIP);
