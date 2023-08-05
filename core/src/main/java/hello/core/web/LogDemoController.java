9ab14401c4bdd8a6f694c45fdfee0e0dfa3cbdf0
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-08-05 18:56:14 +0900

    주석 추가

--- core/src/main/java/hello/core/web/LogDemoController.java
+++ core/src/main/java/hello/core/web/LogDemoController.java
@@ -1,7 +1,6 @@
 package hello.core.web;
 
 import hello.core.common.MyLogger;
-import hello.core.logdemo.LogDemoService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,16 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequiredArgsConstructor
+@RequiredArgsConstructor // 생성자 자동생성해서 의존관계 주입해줘여~
 public class LogDemoController {
     private final LogDemoService logDemoService;
     private final MyLogger myLogger;
 
 
     @RequestMapping("log-demo")
-    @ResponseBody
+    @ResponseBody // html 없이 바로 문자를 바로 보내버리기
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        System.out.println("myLogger = " + myLogger.getClass());
+        //myLogger = class hello.core.common.MyLogger$$SpringCGLIB$$0 가짜 프록시 클래스가 있음
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
