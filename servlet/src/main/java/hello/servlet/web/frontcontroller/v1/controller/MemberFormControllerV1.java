7354f410278dac53c6a343352dcd2810292d531a
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-08-09 00:17:00 +0900

    Front Controller 도입

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);//컨트롤러 -> 뷰 이동할 때 사용
+        dispatcher.forward(request, response); // 다른 서블릿이나 jsp로 이동할 수 있는 기능, 서버 내부에서 다시 호출함
+    }
+}
