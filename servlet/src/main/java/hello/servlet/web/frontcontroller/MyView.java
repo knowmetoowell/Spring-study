c1575631540a38eb44258afbe230310cbffc7fa8
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-08-09 00:16:33 +0900

    Create MyView.java
    인터페이스로 만들지 않고 바로 구현체로 만들었으나 인터페이스로 만드는 게 나음

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.frontcontroller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyView {
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    //뷰를 만들거나 접근하는 행위를 렌더링이라고함
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
