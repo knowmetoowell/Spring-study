948d4b5ced7485efc5f2b41a1e6a58781aaf3623
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-08-20 22:41:04 +0900

    thymeleaf link 코드 추가

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -56,6 +56,13 @@ public class BasicController {
         return "basic/date";
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "/basic/link";
+    }
+
     @GetMapping("/variable")
     public String variable(Model model) {
 
