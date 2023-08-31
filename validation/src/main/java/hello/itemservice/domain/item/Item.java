edd920f1eb2d86d6bd5c0199880c3b796f24a75a
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-08-31 20:39:41 +0900

    validation form 적용

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,30 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000",message = "총합이 10000원이 넘게 입력해주세요.")
 public class Item {
 
+
+//    @NotNull(groups = UpdateCheck.class)//수정할때만
     private Long id;
+
+//    @NotBlank(groups = {UpdateCheck.class, SaveCheck.class})
     private String itemName;
+
+//    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {UpdateCheck.class, SaveCheck.class})
     private Integer price;
+
+//    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+//    @Max(value = 9999, groups = {SaveCheck.class})//저장할때만
     private Integer quantity;
 
     public Item() {
